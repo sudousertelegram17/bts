33b4d8b8ec2e0357a3a916f7e60a76c1658b72a6
shiyal manoj <sudousertelegram17@gmail.com>
2024-07-21 20:26:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "74892754IW0Us8d8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7489275442:AAEsqqvoQu-KcbduarihcCopYuMIW0Us8d8")
 
 #Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "25213176"))
 
 #Your API Hash from my.telegram.o
-API_HASH = os.environ.get("API_HASH", "a720bf5cc1d7c23a69a")
+API_HASH = os.environ.get("API_HASH", "a720bfef89e315cc1d7c2ff628c3a69a")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://ter0.6v96naz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://kidsop:kidsop@cluster0.6v96naz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
